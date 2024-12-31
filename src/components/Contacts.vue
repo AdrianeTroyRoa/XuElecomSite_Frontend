93eb2cfd68c3a478799cd08bb631910a098ad8b0
Troy Roa <roatroy850@gmail.com>
2024-12-31 15:58:30 +0800

    chore: optimized code for contact section

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,39 +1,25 @@
+<script setup lang="ts">
+const inputItems: string[] = ["name", "email", "subject"];
+</script>
 <template>
-  <div class="container mx-auto">
+  <div class="container mx-auto mt-5">
     <div class="flex-col justify-center items-center text-center">
       <h1 class="text-5xl font-bold mb-3">Contact Us</h1>
       <p class="text-xl">Have questions? Submit them here!</p>
     </div>
     <div class="flex justify-center items-center mt-5">
       <form method="POST" name="contact">
-        <input type="hidden" name="form-name" value="contact" />
-        <input
-          class="input input-bordered flex items-center gap-2"
-          style="width: 450px"
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Name"
-          required
-        /><br />
-        <input
-          class="input input-bordered flex items-center gap-2"
-          style="width: 450px"
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Email"
-          required
-        /><br />
-        <input
-          class="input input-bordered flex items-center gap-2"
-          style="width: 450px"
-          type="text"
-          id="subject"
-          name="subject"
-          placeholder="Subject"
-          required
-        /><br />
+        <div v-for="(item, index) in inputItems" :key="index">
+          <input
+            class="input input-bordered flex items-center mb-4"
+            style="width: 450px"
+            type="text"
+            :id="item"
+            :name="item"
+            :placeholder="item.charAt(0).toUpperCase() + item.slice(1)"
+            required
+          />
+        </div>
         <textarea
           class="textarea textarea-bordered flex items-center gap-2"
           rows="5"
