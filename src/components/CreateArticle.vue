dd12f25d603727d8195dbf71e4e6528816de3c9b
Troy Roa <roatroy850@gmail.com>
2025-01-03 23:13:55 +0800

    feat: added a Create Article modal

--- src/components/CreateArticle.vue
+++ src/components/CreateArticle.vue
@@ -1,47 +1,63 @@
 <template>
-  <div class="w-full max-w-xs">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="title">
-          Article Title
-        </label>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="title"
-          type="text"
-          placeholder="Title"
-        />
-      </div>
-      <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="image">
-          Thumbnail Image
-        </label>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="image"
-          type="text"
-          placeholder="Image link"
-        />
-        <div class="flex items-center space-x-4">
-          <hr class="flex-grow border-gray-300" />
-          <span class="text-gray-500">or</span>
-          <hr class="flex-grow border-gray-300" />
-        </div>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          id="image"
-          accept="image/*"
-          type="file"
-        />
-      </div>
-      <div class="text-center">
-        <button
-          class="btn btn-secondary font-bold rounded focus:outline-none focus:shadow-outline"
-          type="button"
-        >
-          Sign In
+  <button class="btn" onclick="my_modal_1.showModal()">open modal</button>
+  <dialog class="modal" id="my_modal_1">
+    <div class="modal-box">
+      <!--Modal close button-->
+      <form method="dialog">
+        <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+          ✕
         </button>
-      </div>
-    </form>
-  </div>
+      </form>
+
+      <!--The form to gather initial article info-->
+      <form
+        class="px-8 pt-6 pb-8 mb-4 mt-5 w-full max-w-lg"
+      >
+        <div class="text-4xl text-center mb-4">Compose New Article</div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="title">
+            Article Title
+          </label>
+          <input
+            class="shadow appearance-none input input-bordered rounded w-full p-4 text-gray-700 focus:outline-none focus:shadow-outline"
+            id="title"
+            type="text"
+            placeholder="Title"
+          />
+        </div>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="image">
+            Thumbnail Image
+          </label>
+          <input
+            class="shadow appearance-none input input-bordered rounded w-full p-4 text-gray-700 focus:outline-none focus:shadow-outline"
+            id="image"
+            type="text"
+            placeholder="Image link"
+          />
+          <div class="flex items-center space-x-4">
+            <hr class="flex-grow border-gray-300" />
+            <span class="text-gray-500">or</span>
+            <hr class="flex-grow border-gray-300" />
+          </div>
+          <div class="text-center mb-6">
+            <input
+              type="file"
+              id="image"
+              accept="image/*"
+              className="file-input w-full max-w-xs shadow shadow-outline"
+            />
+          </div>
+        </div>
+        <div class="text-center">
+          <button
+            class="btn btn-secondary font-bold rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Go to Editor
+          </button>
+        </div>
+      </form>
+    </div>
+  </dialog>
 </template>
