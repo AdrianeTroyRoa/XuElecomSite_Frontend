e6f4aff0bf3a7063cc5abd35f1e65a9bbaa2579a
Troy Roa <roatroy850@gmail.com>
2025-01-03 23:47:34 +0800

    feat: added basic scripting in CreateArticle modal

--- src/components/CreateArticle.vue
+++ src/components/CreateArticle.vue
@@ -10,10 +10,8 @@
       </form>
 
       <!--The form to gather initial article info-->
-      <form
-        class="px-8 pt-6 pb-8 mb-4 mt-5 w-full max-w-lg"
-      >
-        <div class="text-4xl text-center mb-4">Compose New Article</div>
+      <form class="px-8 pt-6 pb-8 mb-4 mt-5 w-full max-w-lg">
+        <div class="text-4xl text-center mb-8">Compose New Article</div>
         <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="title">
             Article Title
@@ -22,6 +20,7 @@
             class="shadow appearance-none input input-bordered rounded w-full p-4 text-gray-700 focus:outline-none focus:shadow-outline"
             id="title"
             type="text"
+            v-model="articleTitle"
             placeholder="Title"
           />
         </div>
@@ -33,6 +32,7 @@
             class="shadow appearance-none input input-bordered rounded w-full p-4 text-gray-700 focus:outline-none focus:shadow-outline"
             id="image"
             type="text"
+            v-model="imageLink"
             placeholder="Image link"
           />
           <div class="flex items-center space-x-4">
@@ -40,12 +40,12 @@
             <span class="text-gray-500">or</span>
             <hr class="flex-grow border-gray-300" />
           </div>
-          <div class="text-center mb-6">
+          <div class="text-center mb-10">
             <input
               type="file"
               id="image"
               accept="image/*"
-              className="file-input w-full max-w-xs shadow shadow-outline"
+              className="file-input shadow shadow-outline"
             />
           </div>
         </div>
@@ -53,6 +53,7 @@
           <button
             class="btn btn-secondary font-bold rounded focus:outline-none focus:shadow-outline"
             type="button"
+            @click="submitInputs"
           >
             Go to Editor
           </button>
@@ -61,3 +62,15 @@
     </div>
   </dialog>
 </template>
+
+<script setup lang="ts">
+console.log("Hello");
+
+const articleTitle = ref("");
+const imageLink = ref("");
+
+function submitInputs() {
+  console.log("article title:", articleTitle.value);
+  console.log("image link", imageLink.value.replace(/\s+/g, ""));
+}
+</script>
