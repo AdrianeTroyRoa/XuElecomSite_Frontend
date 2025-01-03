458f49167470509c4ee903d06de30385971f8373
Troy Roa <roatroy850@gmail.com>
2025-01-04 03:16:17 +0800

    feat: updated Hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,14 +1,43 @@
 <template>
-  <div class="hero bg-base-200 min-h-screen">
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1 class="text-5xl font-bold">XU-CSG Electoral Commission</h1>
-        <p class="py-6">
+  <section class="bg-white h-screen flex items-center justify-center">
+    <div
+      class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12"
+    >
+      <div class="mr-auto place-self-center lg:col-span-7">
+        <h1
+          class="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl"
+        >
+          XU-CSG Electoral Commission
+        </h1>
+        <p class="mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
           Exercising exclusive jurisdiction over all contests relating to the
           elections at Xavier Ateneo
         </p>
-        <button class="btn btn-primary">Omnibus Code 2024</button>
+        <a
+          href="#"
+          class="inline-flex items-center justify-center px-5 py-3 mr-3 font-medium text-center rounded-lg btn btn-primary focus:ring-4 focus:ring-primary-300"
+        >
+          Omnibus Code 2024
+          <svg
+            class="w-5 h-5 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </a>
+        <a
+          href="#contact"
+          class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
+        >
+          Talk to Us
+        </a>
       </div>
     </div>
-  </div>
+  </section>
 </template>
