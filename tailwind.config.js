7a737585dee4e93136a6b8731d815c0e78b7da7b
Troy Roa <roatroy850@gmail.com>
2025-01-14 21:20:14 +0800

    fix: adjusted tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app.vue",
+    "./components/**/*.{js,vue,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  daisyui: {
+    themes: [
+      {
+        xuelecom: {
+          primary: "#303473",
+          secondary: "#F2D649",
+          accent: "#0E0C40",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+};
