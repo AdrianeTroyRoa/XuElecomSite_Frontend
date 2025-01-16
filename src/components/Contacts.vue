3ee93bbe18beafde5819b25288bd71827c9ca845
Troy Roa <roatroy850@gmail.com>
2025-01-16 16:46:47 +0800

    fix: width problem in mobile view

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -2,39 +2,41 @@
 const inputItems: string[] = ["name", "email", "subject"];
 </script>
 <template>
-  <section class="mt-5 min-h-screen flex flex-col items-center justify-center" id="contact">
+  <section
+    class="mt-5 min-h-screen flex flex-col items-center justify-center"
+    id="contact"
+  >
     <div class="flex-col justify-center items-center text-center">
-        <h2
-          class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900"
-        >
-         Contact Us
-        </h2>
-        <p class="font-light text-gray-500 sm:text-xl">
-      Have questions? Submit them here!</p>
+      <h2
+        class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900"
+      >
+        Contact Us
+      </h2>
+      <p class="font-light text-gray-500 sm:text-xl">
+        Have questions? Submit them here!
+      </p>
     </div>
-    <div class="flex justify-center items-center mt-5">
-      <form method="POST" name="contact">
+    <div class="mt-5">
+      <form method="POST" name="contact" class="max-w-sm mx-auto">
         <div v-for="(item, index) in inputItems" :key="index">
-          <input
-            class="input input-bordered flex items-center mb-4"
-            style="width: 450px"
-            type="text"
-            :id="item"
-            :name="item"
-            :placeholder="item.charAt(0).toUpperCase() + item.slice(1)"
-            required
-          />
+          <div class="mb-5">
+            <input
+              type="text"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              :id="item"
+              :name="item"
+              :placeholder="item.charAt(0).toUpperCase() + item.slice(1)"
+              required
+            />
+          </div>
         </div>
         <textarea
-          class="textarea textarea-bordered flex items-center gap-2"
-          rows="5"
           id="message"
-          name="message"
-          placeholder="Message"
-          style="resize: none; width: 450px"
-          required
-        ></textarea
-        ><br /><br />
+          rows="4"
+          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="Leave a comment..."
+        ></textarea>
+        <br /><br />
         <div class="text-center mb-5">
           <button class="btn">Send Message</button>
         </div>
