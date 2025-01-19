7883378b7b9a3524bacc8e7eb533b60aedb3c267
Troy Roa <roatroy850@gmail.com>
2025-01-19 19:34:34 +0800

    feat: added dates in article template

--- pages/a/[id].vue
+++ pages/a/[id].vue
@@ -3,5 +3,27 @@
   <Navbar />
   <article class="prose">
     <h1>Hello, world. This page is for articles</h1>
+    <p>{{formattedDateToday()}}</p>
   </article>
 </template>
+
+<script setup lang="ts">
+// Import the markdown parser
+import { marked } from "marked";
+
+// Reactive content for displaying the datetime
+const dateToday = new Date();
+const dateOptions = {
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: true,
+  weekday: "short",
+  month: "long",
+  day: "numeric",
+  year: "numeric",
+  timeZoneName: "short",
+};
+const formattedDateToday = () => {
+  return dateToday.toLocaleDateString("en-US", dateOptions);
+};
+</script>
