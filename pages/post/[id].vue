0940203d12aa3aca0496bcb62d5539d2b198e689
Troy Roa <roatroy850@gmail.com>
2025-01-20 23:48:35 +0800

    feat: integrated supabase w/ individual posts

--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -4,16 +4,17 @@
   <div class="flex flex-col gap-8 w-full p-4">
     <!--article title-->
     <div class="text-center">
-      <h1 class="text-5xl font-bold">Hello World. This is a title...</h1>
-      <div class="text-sm mt-5 font-light">{{ formattedDateToday() }}</div>
+      <h1 class="text-5xl font-bold">{{ post.title }}</h1>
+      <div class="text-sm mt-5 font-light">
+        {{ formattedDateToday(post.date) }}
+      </div>
     </div>
 
     <!--article image-->
     <div>
       <img
-        src="https://cdn.pixabay.com/photo/2025/01/07/16/54/woman-9317323_960_720.jpg"
+        :src="post.image_link"
         class="w-full max-w-2xl mx-auto rounded-lg shadow-md object-cover"
-        alt="Subway"
       />
     </div>
 
@@ -23,29 +24,7 @@
 
       <!--Article proper-->
       <article class="prose">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis magna
-        ut augue auctor pellentesque eget et tellus. Fusce suscipit vel est id
-        scelerisque. Praesent sem libero, varius sed ullamcorper at, faucibus
-        non nisl. Mauris eu dui vitae ex venenatis bibendum. Vivamus nec nunc
-        quis dui volutpat hendrerit. Suspendisse vulputate, lectus in
-        sollicitudin vehicula, sapien erat egestas erat, vitae interdum diam
-        urna vel dui. Integer eu fermentum magna, eu posuere velit. Praesent sed
-        tellus vestibulum, tincidunt leo vel, vulputate ex. Nullam quis ipsum
-        quis metus tincidunt gravida et non nulla. Duis nec iaculis dolor.
-        Vestibulum dignissim tortor tellus, eget egestas risus blandit nec.
-        Pellentesque vel porta arcu. Ut rutrum condimentum ligula, non viverra
-        sem scelerisque interdum. Donec lacus nulla, blandit eget orci quis,
-        viverra aliquam nisl. Nulla justo nisi, dignissim a sagittis sit amet,
-        scelerisque sed eros. Quisque porta eleifend urna quis vulputate. Nunc
-        luctus tristique arcu nec aliquam. Sed lectus enim, congue non tellus
-        quis, interdum pharetra dolor. Maecenas nec rutrum enim. Vestibulum
-        commodo, lorem sed rutrum maximus, felis lacus pharetra massa, non
-        pulvinar ante libero a libero. Sed pulvinar cursus tellus at mattis.
-        Curabitur efficitur, sem a dictum viverra, turpis libero auctor lorem,
-        non condimentum augue erat id metus. Praesent vulputate velit elit, id
-        sagittis ante sollicitudin a. Etiam accumsan, enim sed tincidunt dictum,
-        tortor odio fringilla lectus, vitae porttitor ante dolor quis dui. Sed
-        dapibus est velit, id finibus mi molestie a.
+        {{ post.content }}
       </article>
 
       <!--Ad space/reminder-->
@@ -58,20 +37,50 @@
 // Import the markdown parser
 import { marked } from "marked";
 
-//Checks if param id is UUID format
+//supabase client and route handler
+const client = useSupabaseClient();
+const route = useRoute();
+
+//handle post proper
+const post = ref([]);
+//for id if valid via supabase entry
+const validUUID = ref(true);
+
+//Checks if param id is UUID format and has supabase entry
 const checkIfUUID = (uuid) => {
   const uuidRegex =
     /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+
   return uuidRegex.test(uuid);
 };
+
+//supabase fetching
+const { data, error } = await client
+  .from("Posts")
+  .select()
+  .eq("id", route.params.id);
+if (error | (data.length == 0)) {
+  console.error("No database entry. Invalid id.");
+  validUUID.value = false;
+} else {
+  console.info("everything is alright!");
+  console.log(data);
+
+  post.value = data.map((el) => ({ ...el, date: el.created_at }));
+  post.value = post.value[0];
+}
+
 definePageMeta({
   validate: async (route) => {
-    return typeof route.params.id === "string" && checkIfUUID(route.params.id);
+    return (
+      typeof route.params.id === "string" &&
+      checkIfUUID(route.params.id) &&
+      validUUID.value
+    );
   },
 });
 
-// Reactive content for displaying the datetime
-const dateToday = new Date();
+// display datetime in appropriate format
 const dateOptions = {
   hour: "2-digit",
   minute: "2-digit",
@@ -82,7 +91,10 @@ const dateOptions = {
   year: "numeric",
   timeZoneName: "short",
 };
-const formattedDateToday = () => {
-  return dateToday.toLocaleDateString("en-US", dateOptions);
+const formattedDateToday = (theDate) => {
+  const date = new Date(theDate);
+  return date.toLocaleDateString("en-US", dateOptions);
 };
+
+//
 </script>
