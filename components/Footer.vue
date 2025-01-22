94ddc7c025b13b824370702aaf6f407550d7e40f
Troy Roa <roatroy850@gmail.com>
2025-01-23 04:32:42 +0800

    feat: footer

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-center bg-primary text-white sm:text-lg text-sm italic font-extralight p-4">
+    &copy; XU-CSG Electoral Commission Systems AY 2425
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,6 @@
     <Hero />
     <Posts />
     <Contacts />
+    <Footer />
   </div>
 </template>
