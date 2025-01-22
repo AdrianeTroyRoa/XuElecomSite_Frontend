e4d7fc0d88807a8bad00af5da3ced3eb78782340
Troy Roa <roatroy850@gmail.com>
2025-01-22 23:25:31 +0800

    feat: progress in modifying sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,31 +1,36 @@
 <template>
   <div class="relative">
     <!-- Hamburger Button -->
-    <button @click="toggleSidebar" class="btn btn-square btn-ghost">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        v-if="isSidebarOpen"
-        fill="white"
-      >
-        <rect y="4" width="24" height="2"></rect>
-        <rect y="11" width="24" height="2"></rect>
-        <rect y="18" width="24" height="2"></rect>
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        v-else
-      >
-        <rect y="4" width="24" height="2"></rect>
-        <rect y="11" width="24" height="2"></rect>
-        <rect y="18" width="24" height="2"></rect>
-      </svg>
-    </button>
+    <div class="fixed top-5 right-4 z-50">
+      <button @click="toggleSidebar" class="btn btn-square btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          v-if="isSidebarOpen"
+          fill="none"
+        >
+          <line x1="18" y1="6" x2="6" y2="18"></line>
+          <line x1="6" y1="6" x2="18" y2="18"></line>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          v-else
+        >
+          <rect y="4" width="24" height="2"></rect>
+          <rect y="11" width="24" height="2"></rect>
+          <rect y="18" width="24" height="2"></rect>
+        </svg>
+      </button>
+    </div>
 
     <!-- Sidebar -->
     <div
@@ -33,31 +38,11 @@
         'translate-x-0': isSidebarOpen,
         'translate-x-full': !isSidebarOpen,
       }"
-      class="text-white fixed top-0 right-0 h-full w-full bg-neutral transform transition-transform duration-300 ease-in-out"
+      class="text-white fixed top-0 right-0 h-full w-full max-w-64 bg-primary transform transition-transform duration-300 ease-in-out"
     >
-      <ul class="p-4 space-y-4 mt-24">
-        <li>
-          <a
-            href="/"
-            class="block p-2 rounded-md hover:bg-secondary hover:text-accent"
-            >Home</a
-          >
-        </li>
-        <li>
-          <a
-            href="/articles"
-            class="block p-2 rounded-md hover:bg-secondary hover:text-accent"
-            >Posts</a
-          >
-        </li>
-        <li>
-          <a
-            href="/questions"
-            class="block p-2 rounded-md hover:bg-secondary hover:text-accent"
-            >Contact</a
-          >
-        </li>
-      </ul>
+      <NuxtLink to="/#home" class="btn btn-ghost text-l">Home</NuxtLink>
+      <NuxtLink to="/#posts" class="btn btn-ghost text-l">Posts</NuxtLink>
+      <NuxtLink to="/#contact" class="btn btn-ghost text-l">Contact</NuxtLink>
     </div>
 
     <!-- Overlay -->
