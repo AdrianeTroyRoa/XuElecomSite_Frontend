6aa993386ad31063e9522246980265e0e448371a
Troy Roa <roatroy850@gmail.com>
2025-01-23 09:01:49 +0800

    fix: corrected some SEO stuff

--- app.vue
+++ app.vue
@@ -11,9 +11,18 @@ useHead({
   title: "XU-CSG EleCom | Official Website",
   meta: [
     {
-      name: "XU-CSG Electoral Commission Site",
+      name: "description",
       content:
-        "Exercising exclusive jurisdiction over all contests relating to the elections at Xavier Ateneo",
+        "Exercising exclusive jurisdiction over all contests relating to the elections at Xavier Ateneo. Stay updated on election guidelines, results, and announcements.",
+    },
+    {
+      name: "author",
+      content: "XU-CSG Electoral Commission Systems Department",
+    },
+    {
+      name: "keywords",
+      content:
+        "XU-CSG, Xavier Ateneo, elections, Electoral Commission, EleCom, Botar Atenista",
     },
   ],
   bodyAttrs: {
@@ -22,11 +31,14 @@ useHead({
 });
 
 useSeoMeta({
-  title: 'XU-CSG EleCom | Official Website',
-  ogTitle: 'XU-CSG EleCom | Official Website',
-  description: 'Exercising exclusive jurisdiction over all contests relating to the elections at Xavier Ateneo. Stay updated on election guidelines, results, and announcements.',
-  ogDescription: 'Exercising exclusive jurisdiction over all contests relating to the elections at Xavier Ateneo. Discover election updates, resources, and official information.',
-  ogImage: 'https://vdhtlmptdfpbtenigido.supabase.co/storage/v1/object/public/post-thumbnails/banner%20(fb).png', // Replace with the actual URL to your logo/banner
-  twitterCard: 'summary_large_image',
-})
+  title: "XU-CSG EleCom | Official Website",
+  ogTitle: "XU-CSG EleCom | Official Website",
+  description:
+    "Exercising exclusive jurisdiction over all contests relating to the elections at Xavier Ateneo. Stay updated on election guidelines, results, and announcements.",
+  ogDescription:
+    "Exercising exclusive jurisdiction over all contests relating to the elections at Xavier Ateneo. Discover election updates, resources, and official information.",
+  ogImage:
+    "https://vdhtlmptdfpbtenigido.supabase.co/storage/v1/object/public/post-thumbnails/banner.png",
+  twitterCard: "summary_large_image",
+});
 </script>
