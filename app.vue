2ca13b975324a5b2129408a9e84be46672e462b9
Troy Roa <roatroy850@gmail.com>
2025-01-23 08:39:26 +0800

    feat: added prod site title

--- app.vue
+++ app.vue
@@ -6,4 +6,18 @@
 onMounted(() => {
   document.documentElement.style.scrollBehavior = "smooth";
 });
+
+useHead({
+  title: "XU-CSG EleCom",
+  meta: [
+    {
+      name: "XU-CSG Electoral Commission Site",
+      content:
+        "Exercising exclusive jurisdiction over all contests relating to the elections at Xavier Ateneo",
+    },
+  ],
+  bodyAttrs: {
+    class: "production",
+  },
+});
 </script>
