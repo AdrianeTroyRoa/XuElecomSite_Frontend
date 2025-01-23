0b68ff962c0ba276216d3c36d548a772c2cefa52
Troy Roa <roatroy850@gmail.com>
2025-01-23 08:53:29 +0800

    feat: added more SEO-related info

--- app.vue
+++ app.vue
@@ -8,7 +8,7 @@ onMounted(() => {
 });
 
 useHead({
-  title: "XU-CSG EleCom",
+  title: "XU-CSG EleCom | Official Website",
   meta: [
     {
       name: "XU-CSG Electoral Commission Site",
@@ -20,4 +20,13 @@ useHead({
     class: "production",
   },
 });
+
+useSeoMeta({
+  title: 'XU-CSG EleCom | Official Website',
+  ogTitle: 'XU-CSG EleCom | Official Website',
+  description: 'Exercising exclusive jurisdiction over all contests relating to the elections at Xavier Ateneo. Stay updated on election guidelines, results, and announcements.',
+  ogDescription: 'Exercising exclusive jurisdiction over all contests relating to the elections at Xavier Ateneo. Discover election updates, resources, and official information.',
+  ogImage: 'https://vdhtlmptdfpbtenigido.supabase.co/storage/v1/object/public/post-thumbnails/banner%20(fb).png', // Replace with the actual URL to your logo/banner
+  twitterCard: 'summary_large_image',
+})
 </script>
