553064808233b5997643a72724dfa2e2c9755940
Troy Roa <roatroy850@gmail.com>
2025-02-15 01:17:09 +0800

    feat: attempt 3 (NEW) on contacts form

--- components/Contacts.vue
+++ components/Contacts.vue
@@ -18,6 +18,7 @@
       <form
         name="contact"
         method="post"
+        action="/success"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
         class="max-w-lg mx-auto"
