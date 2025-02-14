cf60191b0f338ac150bcbdf68baefd94805f8e00
Troy Roa <roatroy850@gmail.com>
2025-02-15 01:00:04 +0800

    feat: attempt 1 (NEW) on contact form

--- components/Contacts.vue
+++ components/Contacts.vue
@@ -15,29 +15,25 @@
       </p>
     </div>
     <div class="mt-5">
-      <form
-        name="kontaka"
-        method="POST"
-        netlify
-        class="max-w-lg mx-auto"
-      >
+      <form @submit.prevent="submitForm" class="max-w-lg mx-auto">
+        <input type="hidden" name="form-name" value="contact" />
         <div v-for="(item, index) in inputItems" :key="index">
           <div class="mb-5">
             <input
               type="text"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block md:w-96 sm:w-64 p-2.5"
-              :id="item"
-              :name="item"
+              v-model="form[item]"
               :placeholder="item.charAt(0).toUpperCase() + item.slice(1)"
               required
             />
           </div>
         </div>
         <textarea
-          id="message"
+          v-model="form.message"
           rows="5"
           class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
           placeholder="Message..."
+          required
         ></textarea>
         <br /><br />
         <div class="text-center mb-5">
@@ -52,4 +48,26 @@
 import bgImage from "@/assets/bgContact.png";
 
 const inputItems = ["name", "email", "subject"];
+
+const form = ref({ name: "", email: "", subject: "" });
+
+const submitForm = async () => {
+  const formData = new FormData();
+
+  formData.append("form-name", "contact");
+
+  for (const key in form.value) {
+    formData.append(key, form.value[key]);
+  }
+
+  try {
+    await fetch("/", {
+      method: "POST",
+      body: formData,
+    });
+    console.info("✅ Form submitted successfully!");
+  } catch (error) {
+    console.error("❌ Form submission failed", error);
+  }
+};
 </script>
